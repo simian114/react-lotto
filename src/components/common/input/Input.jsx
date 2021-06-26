983aee56cf87a46eba342d6f86aca12b6ea8ca61
Sanghyeok Nam <sanam@c3r3s7.42seoul.kr>
2021-06-26 12:10:13 +0900

    fix: styled 컴포넌트

--- src/components/common/input/Input.jsx
+++ src/components/common/input/Input.jsx
@@ -1,7 +1,18 @@
-import { StyledInput } from './Input.styles';
+import styled, { css } from 'styled-components';
 
-const Input = ({ fullWidth }) => {
-  return <StyledInput fullWidth={fullWidth} />;
-};
+const Input = styled.input`
+margin-left: 0.25rem;
+margin-right: 0.25rem;
+text-align: center;
+width: 30px;
+height: 36px;
+${({ fullWidth }) =>
+  fullWidth &&
+  css`
+    width: 100%;
+    margin-right: 0.5rem;
+    margin-left: 0.5rem;
+  `}
+`;
 
 export default Input;
